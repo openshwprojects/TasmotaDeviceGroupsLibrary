321acfdcf227a56311b59a7edafe70b1290c635e
openshwprojects <openshwprojects@gmail.com>
2022-05-12 15:48:45 +0200

    hello udp world

---
+++ src/main_broad.c
@@ -0,0 +1,82 @@
+// Server side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#ifdef WINDOWS
+#include <winsock.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#endif
+    
+#define PORT     4447 
+#define MAXLINE 1024 
+    
+// Driver code 
+int main() { 
+    int len, n; 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char *hello = "Hello from server"; 
+    struct sockaddr_in servaddr, cliaddr; 
+	int iResult;
+	WSADATA wsaData;
+    char bAllow = '1';
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+        
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    iResult = setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, (char *)&bAllow, sizeof(bAllow));
+	if (iResult != 0) {
+		printf("WSAStartup setsockopt: %d\n", iResult);
+		return 1;
+	}
+        
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+        
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+//    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_addr.s_addr  = inet_addr("192.168.0.118");
+    servaddr.sin_port = htons(PORT); 
+        
+
+
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    printf("Server listening\n"); 
+	while(1) {
+    len = sizeof(cliaddr);  //len is value/result 
+    
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                0, ( struct sockaddr *) &cliaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+    printf("Client : %s\n", buffer); 
+    //sendto(sockfd, (const char *)hello, strlen(hello),  
+    //    0, (const struct sockaddr *) &cliaddr, 
+    //        len); 
+    //printf("Hello message sent.\n");  
+    //    
+	}
+    return 0; 
+}
---
+++ src/main_multi.c
@@ -0,0 +1,135 @@
+//
+// Simple listener.c program for UDP multicast
+//
+// Adapted from:
+// http://ntrg.cs.tcd.ie/undergrad/4ba2/multicast/antony/example.html
+//
+// Changes:
+// * Compiles for Windows as well as Linux
+// * Takes the port and group on the command line
+//
+
+#ifdef _WIN32
+    #include <Winsock2.h> // before Windows.h, else Winsock 1 conflict
+    #include <Ws2tcpip.h> // needed for ip_mreq definition for multicast
+    #include <Windows.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <time.h>
+#endif
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MSGBUFSIZE 256
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    u_int yes = 1;
+    struct sockaddr_in addr;
+    struct ip_mreq mreq;
+  
+    const char* group = "239.255.250.250"; 
+	//const char* group = "192.168.0.255";
+    int port = 4447;
+
+
+#ifdef _WIN32
+    //
+    // Initialize Windows Socket API with given VERSION.
+    //
+    WSADATA wsaData;
+    if (WSAStartup(0x0101, &wsaData)) {
+        perror("WSAStartup");
+        return 1;
+    }
+#endif
+
+    // create what looks like an ordinary UDP socket
+    //
+     fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    // allow multiple sockets to use the same PORT number
+    //
+    if (
+        setsockopt(
+            fd, SOL_SOCKET, SO_REUSEADDR, (char*) &yes, sizeof(yes)
+        ) < 0
+    ){
+       perror("Reusing ADDR failed");
+       return 1;
+    }
+
+        // set up destination address
+    //
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); // differs from sender
+    addr.sin_port = htons(port);
+
+    // bind to receive address
+    //
+    if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
+        perror("bind");
+        return 1;
+    }
+
+    // use setsockopt() to request that the kernel join a multicast group
+    //
+    mreq.imr_multiaddr.s_addr = inet_addr(group);
+    //mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    mreq.imr_interface.s_addr = inet_addr("192.168.0.118");
+    if (
+        setsockopt(
+            fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char*) &mreq, sizeof(mreq)
+        ) < 0
+    ){
+        perror("setsockopt");
+        return 1;
+    }
+
+	printf("Waiting for packets\n");
+    // now just enter a read-print loop
+    //
+    while (1) {
+        char msgbuf[MSGBUFSIZE];
+        int addrlen = sizeof(addr);
+        int nbytes = recvfrom(
+            fd,
+            msgbuf,
+            MSGBUFSIZE,
+            0,
+            (struct sockaddr *) &addr,
+            &addrlen
+        );
+        if (nbytes < 0) {
+            perror("recvfrom");
+            return 1;
+        }
+		printf("Received %i bytes\n",nbytes);
+        msgbuf[nbytes] = '\0';
+       // puts(msgbuf);
+     }
+
+#ifdef _WIN32
+    //
+    // Program never actually gets here due to infinite loop that has to be
+    // canceled, but since people on the internet wind up using examples
+    // they find at random in their own code it's good to show what shutting
+    // down cleanly would look like.
+    //
+    WSACleanup();
+#endif
+
+    return 0;
+}
